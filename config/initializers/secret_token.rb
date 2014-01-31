0c9d85223e87e4b36f1ec9f98ca5f5c819b3c0cb
James Courtois <jcourtois@thoughtworks.com>
2014-01-31 12:03:34 -0600

    [aubrey, james, vivian] first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IbeaconWebappDemo::Application.config.secret_key_base = 'fc09f7beea94dbbee22cba51bd12ed5267f5b06c56190cb57629b3c1b1ef190f3bfd5766a66d0214e66762d8803a2fbc37405c0af73da9ae3700b3e58f22f17c'
