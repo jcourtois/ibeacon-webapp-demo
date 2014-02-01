c6414735546acf1cf8f9750e04d37a573522359f
James Courtois <jcourtois@thoughtworks.com>
2014-02-01 10:56:48 -0600

    [james] | resolved routing errors for the visits views

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -42,7 +42,7 @@ class VisitsController < ApplicationController
   def update
     respond_to do |format|
       if @visit.update(visit_params)
-        format.html { redirect_to customer_visits_path, notice: 'Visit was successfully updated.' }
+        format.html { redirect_to customer_visit_path, notice: 'Visit was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
