5f1a2d4abb92bbd89683ba061b1135f2c4cefcd6
Aubrey Chipman <aubrey.chipman@gmail.com>
2014-02-03 14:12:05 -0600

    [vivian, james, aubrey] Refactoring visit controller tests

--- spec/controllers/visits_controller_spec.rb
+++ spec/controllers/visits_controller_spec.rb
@@ -3,34 +3,34 @@ require 'spec_helper'
 describe VisitsController do
   describe '#create' do
     context 'with json request' do
-      before :each do
-        @sometime = DateTime.parse('Mon, 03 Feb 2014 18:13:50 UTC +00:00')
-        @sometime_later = DateTime.parse('Mon, 03 Feb 2014 18:13:50 UTC +00:00') + 2.hours
-        @request_data = {
-          membership_number: 12345,
-          visit: {
-            product_area: 'Wine Cellar',
-            enter_time: @sometime.to_s,
-            exit_time: @sometime_later.to_s
-          }
-        }
-      end
+      let(:request_data) { JSON.parse(File.read('spec/fixtures/wine_cellar_visit_request.json')) }
+      let(:request_entry_time) { DateTime.parse(request_data['visit']['enter_time']) }
+      let(:request_exit_time) { DateTime.parse(request_data['visit']['exit_time']) }
 
       it 'creates a new visit, a new customer, and a new product area' do
-        post :create, @request_data, use_route: :visits_new
+        post :create, request_data, use_route: :visits_new
 
         customer = Customer.find_by(membership_number: 12345)
         customer.should_not be_nil
-        customer.visits.where(enter_time: @sometime, exit_time: @sometime_later).should exist
+        customer.visits.where(
+          enter_time: request_entry_time,
+          exit_time: request_exit_time
+        ).should exist
+
         ProductArea.find_by(name: 'Wine Cellar').should_not be_nil
       end
 
       it 'creates a second visit, using an existing customer, and an existing product area' do
         FactoryGirl.create(:dairy_visit)
 
-        post :create, @request_data, use_route: :visits_new
+        post :create, request_data, use_route: :visits_new
 
-        Customer.find_by(membership_number: 12345).visits.where(enter_time: @sometime, exit_time: @sometime_later).should exist
+        customer = Customer.find_by(membership_number: 12345)
+        customer.visits.where(
+          enter_time: request_entry_time,
+          exit_time: request_exit_time
+        ).should exist
+
         ProductArea.find_by(name: 'Wine Cellar').should_not be_nil
       end
     end
