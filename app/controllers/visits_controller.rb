537f6d3c39049e023adf8093f52465da865a7cbd
Elisa Cutrin <elisacutrin@gmail.com>
2014-02-04 11:13:24 -0600

    James/Elisa - adding logging and removing binding.pry

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,6 +1,7 @@
 class VisitsController < ApplicationController
   before_action :set_visit, only: [:show, :edit, :destroy]
   before_action :set_customer, except: [:create, :update]
+  before_action :logging, only: [:create, :update]
   skip_before_filter :verify_authenticity_token, only: [:create, :new, :update]
 
   def index
@@ -73,5 +74,9 @@ class VisitsController < ApplicationController
 
     def set_customer
       @customer = Customer.find(params[:customer_id])
+    end
+
+    def logging
+      logger.info "REQUEST to visits/create with params: #{params}" 
     end
 end
