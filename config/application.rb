c177101630d65b3762b5970f6edb21273ca37da3
James Courtois <jcourtois@thoughtworks.com>
2014-02-04 18:19:28 -0600

    james | still trying to precompile assets for heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,22 @@ Bundler.require(:default, Rails.env)
 module IbeaconWebappDemo
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+
+    config.assets.precompile << Proc.new do |path|
+      if path =~ /\.(css|js)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('app', 'assets').to_path
+        if full_path.starts_with? app_assets_path
+          puts "including asset: " + full_path
+          true
+        else
+          puts "excluding asset: " + full_path
+          false
+        end
+      else
+        false
+      end
+    end
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
