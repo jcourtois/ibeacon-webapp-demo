853af2ec02b7abcd2b0667951effd88cd73b1452
James Courtois <jcourtois@thoughtworks.com>
2014-02-06 12:04:54 -0600

    james | refactoring logic out of the partials

--- app/helpers/visits_helper.rb
+++ app/helpers/visits_helper.rb
@@ -1,5 +1,23 @@
 module VisitsHelper
   def total_duration visits
-    Time.at(visits.flat_map{|visit| visit.duration}.reduce(:+)).utc.strftime("%H:%M:%S").gsub(/^[0:]*/, '')
+    Time.at(visits.flat_map{ |visit| visit.duration }.reduce(:+)).utc.strftime("%H:%M:%S").gsub(/^[0:]*/, '')
+  end
+
+  def pie_chart_data visits
+    visits.map{|visit| visit.to_pie_chart_json}
+  end
+
+  def step_graph_format time
+    time.to_i * 1000
+  end
+
+  def step_graph_data visits
+    x_axis = visits.flat_map { |visit| [step_graph_format(visit.enter_time), step_graph_format(visit.exit_time)] }
+    y_axis = visits.flat_map { |visit| [visit.product_area.id, visit.product_area.id] }
+    x_axis.zip(y_axis)
+  end
+
+  def step_graph_labels visits
+    visits.map{ |visit| [visit.product_area.id, visit.product_area.name] }
   end
 end
--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,44 +1,26 @@
 class Visit < ActiveRecord::Base
-  include ActionView::Helpers::DateHelper
-
   belongs_to :customer
   belongs_to :product_area
 
-  def exit_time_or_ongoing
-    exit_time || Time.now - 6.hours
-  end
-
   def duration
     exit_time_or_ongoing - enter_time
+  end
+
+  def exit_time_or_ongoing
+    exit_time || Time.now - 6.hours
   end
 
   def string_numeric_duration
     Time.at(duration).utc.strftime("%H:%M:%S").gsub(/^00:0|^00:|^0/, '')
   end
 
-  def verbal_duration
-    if exit_time
-      distance_of_time_in_words(exit_time, enter_time, include_seconds: true)
-    else
-      distance_of_time_in_words_to_now(enter_time, include_seconds: true)
-    end
-  end
-
   def to_pie_chart_json
     {"value" => duration, "color" => product_area.color}
   end
 
-  def to_step_graph_times position
-    position == 'enter' ? enter_time.to_i * 1000 : exit_time.to_i * 1000
+  def to_step_graph_time_pairs
+    [step_graph_format(enter_time), step_graph_format(exit_time)]
   end
-
-  def to_step_graph_product_areas
-    product_area.id
-  end
-
-  def to_step_graph_labels
-    [product_area.id, product_area.name]
-	end
 
 	def formatted_enter_time
     enter_time.strftime("%l:%M%P").strip
