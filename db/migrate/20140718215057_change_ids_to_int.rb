8c075352413b641c943ab3b97ef006b877c226c6
Sarah Cnota <scnota@gmail.com>
2014-07-18 17:05:09 -0500

    sarah/vivian | fix migration again

--- db/migrate/20140718215057_change_ids_to_int.rb
+++ db/migrate/20140718215057_change_ids_to_int.rb
@@ -1,8 +1,8 @@
 class ChangeIdsToInt < ActiveRecord::Migration
   def up
     if Rails.env == 'production'
-      change_column :coupons, :product_area_id, 'interger USING CAST(product_area_id AS integer)'
-      change_column :events, :coupon_id, 'interger USING CAST(coupon_id AS integer)'
+      change_column :coupons, :product_area_id, 'integer USING CAST(product_area_id AS integer)'
+      change_column :events, :coupon_id, 'integer USING CAST(coupon_id AS integer)'
     else
       change_column :coupons, :product_area_id, :int
       change_column :events, :coupon_id, :int
